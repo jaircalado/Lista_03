b5b164bb357a976370f1f36e3f7bb0de1c2203bb
Jair <jair.calado@upe.br>
2026-05-12 18:10:20 -0300

    1º Exercício da lista 3

--- src/kth_to_last.py
+++ src/kth_to_last.py
@@ -2,4 +2,5 @@ from src.my_node import MyNode
 
 
 def kth_to_last(head: MyNode, k: int) -> int:
-    raise NotImplementedError
+    if head is None or k <= 1:
+        retur 
--- src/merge_lists.py
+++ src/merge_lists.py
@@ -2,4 +2,17 @@ from src.my_node import MyNode
 
 
 def merge_lists(lista1: MyNode, lista2: MyNode) -> MyNode:
-    raise NotImplementedError
+    if lista1 is None:
+        return lista2
+    
+    if lista2 is None:
+        return lista1
+
+    if lista1.value < lista2.value:
+        merged_head = lista1
+        merged_head.next = merge_lists(lista1.next, lista2)
+    else:
+        merged_head = lista2
+        merged_head.next = merge_lists(lista1, lista2.next)
+
+    return merged_head
